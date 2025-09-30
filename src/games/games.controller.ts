0719531a6fb986cbb2d48702f20155d63e3e0f06
Nikolett Tyukasz <nikolett.tyukasz@nixs.com>
2025-09-30 17:51:35 +0200

    basic game creation and joining

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Patch, Post, UseGuards } from '@nestjs/common';
+import { PlayerRolesEnum } from 'src/enums/playerRoles.enum';
+import { GetUser } from 'src/users/get-user.decorator';
+import { User } from 'src/users/users.entity';
+import { Game } from './games.entity';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.authGuards';
+import { GamesService } from './games.service';
 
 @Controller('games')
-export class GamesController {}
+@UseGuards(JwtAuthGuard)
+export class GamesController {
+  constructor(private readonly gamesService: GamesService) {}
+
+  @Post('game')
+  createGame(
+    @GetUser() player: User,
+    @Body() gameSetup: any,
+    @Body() playerRole: PlayerRolesEnum,
+  ): Promise<Game> {
+    return this.gamesService.createGame(player, gameSetup, playerRole);
+  }
+
+  @Patch('game')
+  joinGame(
+    @GetUser() player: User,
+    @Body() gameId: string,
+    @Body() playerRole: PlayerRolesEnum,
+  ): Promise<Game> {
+    return this.gamesService.joinGame(player, gameId, playerRole);
+  }
+}
