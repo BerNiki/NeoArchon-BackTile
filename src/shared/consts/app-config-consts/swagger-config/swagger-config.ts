6b996df702376c78601e7e320824e5f42b413b96
Nikolett Tyukasz <nikolett.tyukasz@nixs.com>
2025-11-19 12:06:24 +0100

    pipelinefix

--- src/shared/consts/app-config-consts/swagger-config/swagger-config.ts
+++ src/shared/consts/app-config-consts/swagger-config/swagger-config.ts
@@ -4,6 +4,6 @@ import { version as softwareNumber } from '../../../../../package.json';
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('Archon Backend')
   .setDescription('The Archon API documentation')
-  .setVersion(softwareNumber as string)
+  .setVersion(softwareNumber)
   .addBearerAuth()
   .build();
