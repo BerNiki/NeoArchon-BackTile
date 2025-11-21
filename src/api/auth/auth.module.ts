e0e447a87f7d8a8dd876ac487705fac49cf12bea
Nikolett Tyukasz <nikolett.tyukasz@nixs.com>
2025-11-21 13:54:58 +0100

    adds rate limiting

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -6,11 +6,18 @@ import { User } from 'src/api/users/entities/users.entity';
 import { UsersService } from 'src/api/users/users.service';
 import { ConfigModule } from 'src/config/config.module';
 import { jwtKeysProvider } from './utils/jwt.keys.provider';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Global()
 @Module({
   imports: [TypeOrmModule.forFeature([User]), ConfigModule],
-  providers: [AuthService, UsersService, jwtKeysProvider],
+  providers: [
+    AuthService,
+    UsersService,
+    jwtKeysProvider,
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+  ],
   controllers: [AuthController],
   exports: [AuthService, jwtKeysProvider],
 })
--- src/app.module.ts
+++ src/app.module.ts
@@ -16,8 +16,11 @@ import { CorrelationIdMiddleware } from './common/middleware/correlation-id.midd
 import {
   CONFIG_MODULE_OPTIONS,
   LOGGER_MODULE_OPTIONS,
+  THROTTLER_MODULE_OPTIONS,
   TYPEORM_MODULE_OPTIONS,
 } from './shared/consts/app-module-consts/appModuleOptions';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -29,8 +32,10 @@ import {
     NestConfigModule.forRoot(CONFIG_MODULE_OPTIONS),
     TypeOrmModule.forRootAsync(TYPEORM_MODULE_OPTIONS),
     LoggerModule.forRoot(LOGGER_MODULE_OPTIONS),
+    ThrottlerModule.forRoot(THROTTLER_MODULE_OPTIONS),
     HealthModule,
   ],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
